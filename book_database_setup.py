e0aa1b01e82ee9b33371bad65c2fd338dabc926f
vagrant <vagrant@vagrant.vm>
2017-08-01 16:39:21 +0000

    Setting up the bookcatalog database with sqlalchemy with bookcategory and book table!!!

---
+++ book_database_setup.py
@@ -0,0 +1,42 @@
+"""setting up the database for bookcatalog web-application. 
+
+"""
+
+
+#importing default os, sys and specific packages of sqlalchemy module to build the database 
+
+import os 
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+class BookCategory(Base):
+
+	"""creating Bookcategory class and table in the database."""
+
+	__tablename__ = 'bookcategory'
+
+	id = Column(Integer,primary_key=True)
+	name =  Column(String(250),nullable=False)
+
+class Book(Base):
+
+	""" creating Book class and table in the database. """
+
+	__tablename__ = 'book'
+
+	name = Column(String(300),nullable = False)
+	id = Column(Integer,primary_key = True)
+	author = Column(String(400),nullable = False)
+	# ratings =  Column(float, nullable = False) cannot make the float datatype declaration in sqlalchemy
+	description = Column(String(500),nullable = False)
+	reviews = Column(String(500))
+	bookcategory_id = Column(Integer,ForeignKey('bookcategory.id'))
+	bookcategory = relationship(BookCategory)
+
+engine = create_engine('sqlite:///bookcatalog.db')
+Base.metadata.create_all(engine)
