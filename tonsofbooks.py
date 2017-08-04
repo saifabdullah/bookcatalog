2416e03a5246bdbe0dfc0d84bd0abe8c51b044e2
vagrant <vagrant@vagrant.vm>
2017-08-04 02:04:38 +0000

    Modified tonsofbooks.py to add some random book data. Removed the database and run database setup again before running tonsofbooks.py

--- tonsofbooks.py
+++ tonsofbooks.py
@@ -18,9 +18,89 @@ category1= BookCategory(name="History & autobiograpy")
 session.add(category1)
 session.commit()
 
-book1  = Book(name="A Long Walk to Freedom", author="x", description= "y",   reviews= "z", bookcategory= category1 ) 
+
+
+
+book2  = Book(name="A Long Walk to Freedom", author="x", description= "y",   reviews= "z", bookcategory= category1 ) 
+
+session.add(book2)
+session.commit()
+
+book3  = Book(name="A Long Walk to Freedom", author="x", description= "y",   reviews= "z", bookcategory= category1 ) 
+
+session.add(book3)
+session.commit()
+
+
+book4  = Book(name="A Long Walk to Freedom", author="x", description= "y",   reviews= "z", bookcategory= category1 ) 
+
+session.add(book4)
+session.commit()
+
+
+book5  = Book(name="A Long Walk to Freedom", author="x", description= "y",   reviews= "z", bookcategory= category1 ) 
+
+session.add(book5)
+session.commit()
+
+
+category2= BookCategory(name="Technology")
+
+session.add(category2)
+session.commit()
+
+
+book1 = Book(name="Learn Python the hard way",author="x",description="y",reviews="z", bookcategory=category2)
+session.add(book1)
+session.commit()
+
+
+book2 = Book(name="Learn Python the hard way",author="x",description="y",reviews="z", bookcategory=category2)
+session.add(book2)
+session.commit()
+
+book3 = Book(name="Learn Python the hard way",author="x",description="y",reviews="z", bookcategory=category2)
+session.add(book3)
+session.commit()
+
+book4 = Book(name="Learn Python the hard way",author="x",description="y",reviews="z", bookcategory=category2)
+session.add(book4)
+session.commit()
+
+book5 = Book(name="Learn Python the hard way",author="x",description="y",reviews="z", bookcategory=category2)
+session.add(book5)
+session.commit()
+
+
+category3= BookCategory(name="Language & Culture")
+
+session.add(category3)
+session.commit()
+
+book1= Book(name="Letters to My daughter",author="x", description="y", reviews="z",bookcategory=category3)
 
 session.add(book1)
 session.commit()
+
+book2= Book(name="Letters to My daughter",author="x", description="y", reviews="z",bookcategory=category3)
+
+session.add(book2)
+session.commit()
+
+book3= Book(name="Letters to My daughter",author="x", description="y", reviews="z",bookcategory=category3)
+
+session.add(book3)
+session.commit()
+
+book4= Book(name="Letters to My daughter",author="x", description="y", reviews="z",bookcategory=category3)
+
+session.add(book4)
+session.commit()
+
+book5= Book(name="Letters to My daughter",author="x", description="y", reviews="z",bookcategory=category3)
+
+session.add(book5)
+session.commit()
+
 
 print "added books!"
