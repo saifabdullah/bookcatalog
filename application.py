920c959c5433b730558565188f9022609a99bd68
saifabdullah <saifab@gmail.com>
2017-08-13 21:03:43 +0600

    Created a homepage for the catalog applicaiton where all categories and book are displayed.

--- application.py
+++ application.py
@@ -1,38 +1,33 @@
-from flask import Flask ,render_template
+from flask import Flask, render_template, request, redirect, url_for
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from book_database_setup import Base,  BookCategory, Book
-
+ 
 app = Flask(__name__)
-
+ 
 engine = create_engine('sqlite:///bookcatalog.db')
 Base.metadata.bind = engine
-
+ 
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
+ 
+@app.route('/cataloghome/')
+@app.route('/')
+
+def home():
+	categories =session.query(BookCategory).all()
+	items = session.query(Book).all()
+	return render_template('home.html',categories=categories,items=items)
 
 
-
-
-
-
-
-    
+     
 @app.route('/cataloghome/<int:bookcategory_id>/')
-
+ 
 def cataloghome(bookcategory_id):
-	
-	bookcategory = session.query(BookCategory).filter_by(id=bookcategory_id).one()
-	items = session.query(Book).filter_by(bookcategory_id=bookcategory.id)
-	
-	
-		
-	return render_template('catalog.html', bookcategory=bookcategory,items=items)
-
-@app.route('/cataloghome/<int:bookcategory_id>/newbook')
-
-def newBook(bookcategory_id):
-    return "page to create a new book."
+ 	
+  	bookcategory = session.query(BookCategory).filter_by(id=bookcategory_id).one()
+  	items = session.query(Book).filter_by(bookcategory_id=bookcategory.id)
+  	return render_template('catalog.html', bookcategory=bookcategory,items=items)
 
 
 @app.route('/cataloghome/<int:bookcategory_id>/<int:id>/editbook')
