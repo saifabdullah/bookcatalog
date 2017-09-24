b3a59d6ce237b7b91e073d4bc3a8c5986162d0f5
saifabdullah <saifab@gmail.com>
2017-09-24 17:42:54 +0600

    Removed tabs from code and used 4 spaces instead to indent

--- book_database_setup.py
+++ book_database_setup.py
@@ -1,6 +1,6 @@
-#importing default os, sys and specific packages of sqlalchemy module to build the database 
+#importing default os, sys and specific packages of sqlalchemy module to build the database
 
-import os 
+import os
 import sys
 from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
@@ -10,53 +10,50 @@ from sqlalchemy import create_engine
 Base = declarative_base()
 
 class User(Base):
-	__tablename__  = 'user'
+    __tablename__ = 'user'
 
-	id  = Column(Integer, primary_key=True)
-	name = Column(String(250), nullable=False)
-	email = Column(String(250), nullable=False)
-	picture = Column(String(250))
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    picture = Column(String(250))
 
 class BookCategory(Base):
 
-	#creating Bookcategory class and table in the database.
+    #creating Bookcategory class and table in the database.
+    __tablename__ = 'bookcategory'
 
-	__tablename__ = 'bookcategory'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
-	id = Column(Integer,primary_key=True)
-	name =  Column(String(250),nullable=False)
-	user_id = Column(Integer, ForeignKey('user.id'))
-	user = relationship(User)
 
-	
 
 class Book(Base):
 
-	""" creating Book class and table in the database. """
+    #creating Book class and table in the database.
+    __tablename__ = 'book'
 
-	__tablename__ = 'book'
+    name = Column(String(300),nullable = False)
+    id = Column(Integer,primary_key = True)
+    author = Column(String(400),nullable = False)
+    description = Column(String(500),nullable = False)
+    reviews = Column(String(500))
+    bookcategory_id = Column(Integer,ForeignKey('bookcategory.id'))
+    bookcategory = relationship(BookCategory)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
-	name = Column(String(300),nullable = False)
-	id = Column(Integer,primary_key = True)
-	author = Column(String(400),nullable = False)
-	# ratings =  Column(float, nullable = False) cannot make the float datatype declaration in sqlalchemy
-	description = Column(String(500),nullable = False)
-	reviews = Column(String(500))
-	bookcategory_id = Column(Integer,ForeignKey('bookcategory.id'))
-	bookcategory = relationship(BookCategory)
-	user_id = Column(Integer, ForeignKey('user.id'))
-	user = relationship(User)
-
-	@property
-	def serialize(self):
-		# Returns object data in easily serializeable format
-		return {
-			'name'	: self.name,
-			'id' : self.id,
-			'author' : self.author,
-			'description' : self.description,
-			'reviews' : self.reviews,
-		}
+    @property
+    def serialize(self):
+        # Returns object data in easily serializeable format
+        return {
+        'name'  : self.name,
+        'id' : self.id,
+        'author' : self.author,
+        'description' : self.description,
+        'reviews' : self.reviews,
+        }
 
 engine = create_engine('sqlite:///bookcatalog.db')
 Base.metadata.create_all(engine)
